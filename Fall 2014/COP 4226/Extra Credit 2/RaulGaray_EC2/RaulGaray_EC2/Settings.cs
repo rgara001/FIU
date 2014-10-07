fe93bf73238583f8ced9908e270fda779d96f4e4
Raul-PC <raulg03@gmail.com>
2014-10-07 01:07:52 -0400

    COP 4226 Homework 1 created with user settings declared in Properties

--- Fall 2014/COP 4226/Extra Credit 2/RaulGaray_EC2/RaulGaray_EC2/Settings.cs
+++ Fall 2014/COP 4226/Extra Credit 2/RaulGaray_EC2/RaulGaray_EC2/Settings.cs
@@ -9,7 +9,7 @@
     internal sealed partial class Settings {
         
         public Settings() {
-            // // To add event handlers for saving and changing settings, uncomment the lines below:
+            // // To add event handle  saving and changing settings, uncomment the lines below:
             //
             // this.SettingChanging += this.SettingChangingEventHandler;
             //
